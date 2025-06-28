af75e03d3b45e900d617d38f6e5a451e99a9f2d3
Yatharth <97800277+yatharth-2906@users.noreply.github.com>
2025-06-28 11:38:01 +0530

    Updated CORS

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,6 @@ const corsOptions = {
 app.use(express.json());
 app.use(express.text());
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 
 // Routers
